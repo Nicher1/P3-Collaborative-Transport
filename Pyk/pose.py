44c31aaf24ce76fa09c59795fbb15839c413dc1d
mBoegh <boegh.skole@gmail.com>
2022-11-17 13:16:35 +0100

    bæ
    Denne version er blot en test mellem iterationer. Den indeholder forsøg på at få cv2 til at loade billeder captured med pyk4a. Derudover indeholder den et library kaldet "helpers" som er nødvendigt til at favelægge dybdekameraets data.

--- Pyk/pose.py
+++ Pyk/pose.py
@@ -1,3 +1,4 @@
+import time
 from pyexpat import model
 import cv2 as cv
 import numpy as np
@@ -6,6 +7,8 @@ import pyk4a as pyk
 from pyk4a import PyK4A, connected_device_count
 from pyk4a import Config
 from time import perf_counter
+from helpers import colorize
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -28,61 +31,69 @@ def main():
         Config(
             color_resolution=pyk.ColorResolution.RES_720P,
             depth_mode=pyk.DepthMode.NFOV_UNBINNED,
-            synchronized_images_only=True   
+            synchronized_images_only=True,
+            camera_fps=pyk.FPS.FPS_5
+
         )
     )
     k4a.start()
-h
     k4a.whitebalance = 4500
     assert k4a.whitebalance==4500
     k4a.whitebalance = 4510
     assert k4a.whitebalance == 4510
 
-    cap = cv.VideoCapture(0)
+    while True:
+        k4aCapture = k4a.get_capture()
+        if np.any(k4aCapture.depth):
+            cv.imshow("k4a", colorize(k4aCapture.depth, (None, 5000), cv.COLORMAP_HSV))
+            cv.waitKey(0)
+            cap = cv.imread(k4aCapture.depth)
+            #cap = cv.VideoCapture(k4aCapture.depth)
+            break
 
-    with mp_hands.Hands(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as hands:
-        with mp_pose.Pose(
-        model_complexity=0,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as pose:
-        
-            while cap.isOpened():
-                success, image=cap.read()
-                if not success:
-                    print("Ignoring empty camera frame.")
-                    continue
-            
-                image.flags.writeable=False
-                image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-                resultHands = hands.process(image)
-                result = pose.process(image)
+            with mp_hands.Hands(
+                model_complexity=0,
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as hands:
+                with mp_pose.Pose(
+                model_complexity=0,
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as pose:
 
-                image.flags.writeable=True
-                image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
-                if resultHands.multi_hand_landmarks:
-                    for hand_landmarks in resultHands.multi_hand_landmarks:
+                    while cap.isOpened():
+                        success, image=cap.read()
+                        if not success:
+                            print("Ignoring empty camera frame.")
+                            continue
+
+                        image.flags.writeable=False
+                        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+                        resultHands = hands.process(image)
+                        result = pose.process(image)
+
+                        image.flags.writeable=True
+                        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+                        if resultHands.multi_hand_landmarks:
+                            for hand_landmarks in resultHands.multi_hand_landmarks:
+                                mp_drawing.draw_landmarks(
+                                    image,
+                                    hand_landmarks,
+                                    mp_hands.HAND_CONNECTIONS,
+                                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                                    mp_drawing_styles.get_default_hand_connections_style())
                         mp_drawing.draw_landmarks(
                             image,
-                            hand_landmarks,
-                            mp_hands.HAND_CONNECTIONS,
-                            mp_drawing_styles.get_default_hand_landmarks_style(),
-                            mp_drawing_styles.get_default_hand_connections_style())
-                mp_drawing.draw_landmarks(
-                    image,
-                    result.pose_landmarks,
-                    mp_pose.POSE_CONNECTIONS,
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+                            result.pose_landmarks,
+                            mp_pose.POSE_CONNECTIONS,
+                            landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
 
-                # your code here
+                        # your code here
 
 
-                cv.imshow('MediaPipe', cv.flip(image,1))
-                if cv.waitKey(5) & 0xFF == 27:
-                    break
+                        cv.imshow('MediaPipe', cv.flip(image,1))
+                        if cv.waitKey(5) & 0xFF == 27:
+                            break
 
 if __name__=="__main__":
     main()
