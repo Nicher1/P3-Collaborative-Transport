fdecf16fb3e693dacf75bbbde87b8a853fd713fd
mBoegh <boegh.skole@gmail.com>
2022-11-23 13:38:48 +0100

    fisk

--- Pyk/pyk.py
+++ Pyk/pyk.py
@@ -7,13 +7,22 @@ import mediapipe as mp
 from helpers import colorize
 import pyk4a as pyk
 
+#####################################################################
+###########################     SETUP     ########################### 
+#####################################################################
 
+# Setup constants for use in main(), which needs to be defined only once
+InnerThresh = 0.05
+OuterThresh = 0.1
+col = (0, 255, 0)
+
+# Definition of mediapipe tracking solutions and drawing styles
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 mp_pose = mp.solutions.pose
 
-
+# Detection of the connected Azure Kinect camera
 cnt = connected_device_count()
 if not cnt:
     print("No devices available")
@@ -25,26 +34,39 @@ for device_id in range(cnt):
     print(f"{device_id}: {device.serial}")
     device.close()
 
-
-
-def main():
-    k4a = PyK4A(
+# Camera configuration
+k4a = PyK4A(
         Config(
         color_resolution=pyk.ColorResolution.RES_720P,
         camera_fps=pyk.FPS.FPS_5,
         depth_mode=pyk.DepthMode.NFOV_UNBINNED,
         synchronized_images_only=True,
-    # pyk.ImageFormat
         )
     )
 
+# Hand detection setup params
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+
+mpDraw = mp.solutions.drawing_utils
+# Detect the joint of hands and return the position of finger tips
+# https://google.github.io/mediapipe/solutions/hands.html
+
+
+#####################################################################
+###########################   FUNCTIONS   ########################### 
+#####################################################################
+
+# primary function containing all main code
+def main():
     k4a.start()
     k4a.whitebalance = 4500
     assert k4a.whitebalance==4500
     k4a.whitebalance = 4510
     assert k4a.whitebalance == 4510
-
-    col = (0, 255, 0)
 
     while True:
         k4aCapture = k4a.get_capture()
@@ -55,61 +77,146 @@ def main():
 
             start = perf_counter()
             
-            with mp_pose.Pose(
-            model_complexity=0,
-            static_image_mode=True,
-            enable_segmentation=True,
-            min_detection_confidence=0.7) as pose:
+            res, fingertips, diff = detectHands(cap)
 
-                result = pose.process(cap)
+            end = perf_counter()
+            print(end-start)
+            cv.imshow("res",res)
+            cv.waitKey(1)
 
-                LandResults = []
-                Idx = []
-                try:
-                    for id, lm in enumerate(result.pose_landmarks.landmark):
-                        LandResults.append([])
-                        x= int(lm.x*cap.shape[1])
-                        y= int(lm.y*cap.shape[0])
-                        LandResults[id].append(y)
-                        LandResults[id].append(x)
-                        Idx.append(id)
+# function for hand detection. Also included is processing of the wrists relation to eachother and the middlepoint in between the wrists positional error regarding that of the i
+def detectHands(Input_img):
+    # Hand detection    
+    forHand = cv.cvtColor(Input_img, cv.COLOR_BGR2RGB)
+    results = hands.process(forHand)
+    #print(results.multi_hand_landmarks)
 
-                    DrawSelect =[LandResults[15],LandResults[16]]
+    # Unpacking Input_img shape pro
+    h, w, c = Input_img.shape
+    fingertips = np.zeros((5,2))
+
+    Center = [int(h/2), int(w/2)]
+    
+    OuterBox = [int(np.rint(Center[0]*(1-OuterThresh))),int(np.rint(Center[1]*(1-OuterThresh))), int(np.rint(Center[0]*(1+OuterThresh))),int(np.rint(Center[1]*(1+OuterThresh)))]
+    InnerBox = [int(np.rint(Center[0]*(1-InnerThresh))),int(np.rint(Center[1]*(1-InnerThresh))), int(np.rint(Center[0]*(1+InnerThresh))),int(np.rint(Center[1]*(1+InnerThresh)))]
+
+    
+    col = 0
+    if results.multi_hand_landmarks:
+        handPos = []
+        for handLms in results.multi_hand_landmarks:
+            col += 2
+            for id, hand in enumerate(handLms.landmark):
+                #print(id,hand)
+                cx, cy = int(hand.x *w), int(hand.y*h)
+
+                if id in [0]:
+                    cv.circle(forHand, (cx,cy), 4, (int(255/20)*(col*4), 255-int(255/20)*(col*5), 255), cv.FILLED)
+                    handPos.append(cy)
+                    handPos.append(cx)
+            #mpDraw.draw_landmarks(forHand, handLms, mpHands.HAND_CONNECTIONS)
+        #print(handPos)
+        
+        if len(handPos) == 4:
+            meany = int((handPos[0] + handPos[2])/2)
+            meanx = int((handPos[1] + handPos[3])/2)
+            cv.circle(forHand, (meanx, meany), 4, (0, 255, 0), -1)
+            centerDiff = [Center[0]-meanx, Center[1]-meanx] # Contains y and x coordinate difference between hands mean and center respectively
+            print(centerDiff)
 
 
+        for i in range(5):
+            fingertips[i][0] = handLms.landmark[(i+1)*4].x * w
+            fingertips[i][1] = handLms.landmark[(i+1)*4].y * h
 
-                    LandResults = []
-                    Idx = []
-                    for id, lm in enumerate(result.pose_landmarks.landmark):
-                        LandResults.append([])
-                        x= int(lm.x*cap.shape[1])
-                        y= int(lm.y*cap.shape[0])
-                        LandResults[id].append(y)
-                        LandResults[id].append(x)
-                        Idx.append(id)
-
-                    Center = [cap.shape[0]/2,cap.shape[1]/2]
-                    averaveX = int(np.rint((LandResults[15][1]+LandResults[16][1])/2))
-                    averageY = int(np.rint((LandResults[15][0]+LandResults[16][0])/2))
-
-                    if averaveX > 288 and averaveX < 352 and averageY > 216 and averageY < 264:
-                        if averaveX > 304 and averaveX < 336 and averageY > 228 and averageY < 252:
-                            print("blå")
-                        else: 
-                            print("rød")
-                    else:
-                        print(Center[0]-averageY, Center[1]-averaveX)
-
-                    
-                except AttributeError:
-                    continue
-                end = perf_counter()
-                print(end-start)
-                cv.imshow("Result", cap)
-               # cv.imshow("depth", colorize(k4aCapture.depth, (None, 5000), cv.COLORMAP_HSV))
-                cv.waitKey(1)
+        
+        
+        
+        if meanx > Center[1]+(1-OuterThresh) and meanx < Center[1]+(1+OuterThresh) and meany > Center[0]-(1+OuterThresh) and meany < Center[0]+(1+OuterThresh):
+            if meanx > Center[1]+(1-InnerThresh) and meanx < Center[1]+(1+InnerThresh) and meany > Center[0]-(1+InnerThresh) and meany < Center[0]+(1+InnerThresh):
+                print("blå")
+                centerDiff = 0
+            else: 
+                print("rød")
+                centerDiff = 0
+        else:
+            print(centerDiff)
+            return forHand, fingertips, centerDiff
+            
+    return forHand, fingertips, centerDiff
 
 
 if __name__ == '__main__':
     main()
-    
+
+
+######################################################################
+##########################   OLD SNIPPETS   ########################## 
+######################################################################
+
+'''
+with mp_pose.Pose(
+model_complexity=0,
+static_image_mode=True,
+enable_segmentation=True,
+min_detection_confidence=0.85) as pose:
+
+    result = pose.process(cap)
+
+    LandResults = []
+    Idx = []
+    try:
+        for id, lm in enumerate(result.pose_landmarks.landmark):
+            LandResults.append([])
+            x= int(lm.x*cap.shape[1])
+            y= int(lm.y*cap.shape[0])
+            LandResults[id].append(y)
+            LandResults[id].append(x)
+            Idx.append(id)
+
+        DrawSelect =[LandResults[15],LandResults[16]]
+
+
+
+        LandResults = []
+        Idx = []
+        for id, lm in enumerate(result.pose_landmarks.landmark):
+            LandResults.append([])
+            x= int(lm.x*cap.shape[1])
+            y= int(lm.y*cap.shape[0])
+            LandResults[id].append(y)
+            LandResults[id].append(x)
+            Idx.append(id)
+
+        Center = [cap.shape[0]/2, cap.shape[1]/2]
+        OuterBox = [int(np.rint(Center[0]*(1-OuterThresh))),int(np.rint(Center[1]*(1-OuterThresh))), int(np.rint(Center[0]*(1+OuterThresh))),int(np.rint(Center[1]*(1+OuterThresh)))]
+        InnerBox = [int(np.rint(Center[0]*(1-InnerThresh))),int(np.rint(Center[1]*(1-InnerThresh))), int(np.rint(Center[0]*(1+InnerThresh))),int(np.rint(Center[1]*(1+InnerThresh)))]
+
+
+        Center = [cap.shape[0]/2,cap.shape[1]/2]
+        averaveX = int(np.rint((LandResults[15][1]+LandResults[16][1])/2))
+        averageY = int(np.rint((LandResults[15][0]+LandResults[16][0])/2))
+
+        cv.rectangle(cap, (OuterBox[1],OuterBox[0]),(OuterBox[3],OuterBox[2]),(0,0,255),-1)
+        cv.rectangle(cap, (InnerBox[1],InnerBox[0]), (InnerBox[3],InnerBox[2]),(255,0,0),-1)
+        cv.circle(cap, (averaveX, averageY), 5, (0,255,0), -1)
+
+        if averaveX > 288 and averaveX < 352 and averageY > 216 and averageY < 264:
+            if averaveX > 304 and averaveX < 336 and averageY > 228 and averageY < 252:
+                print("blå")
+            else: 
+                print("rød")
+        else:
+            print(Center[0]-averageY, Center[1]-averaveX)
+        
+        for i in range(len(DrawSelect)):
+            cv.circle(cap, (DrawSelect[i][1], DrawSelect[i][0]), 5, (0, 255, 0), -1)
+        
+    except AttributeError:
+        continue
+    end = perf_counter()
+    print(end-start)
+    cv.imshow("Result", cap)
+    # cv.imshow("depth", colorize(k4aCapture.depth, (None, 5000), cv.COLORMAP_HSV))
+    cv.waitKey(1)
+'''
