30899f41b18d1dc8854217e7beb7d9a865031dcc
EliasTDam <edam21@student.aau.dk>
2022-11-24 11:43:35 +0100

    Cleaned up code. Final product, ready to be made into a library.

--- UR10/Rail/main.py
+++ UR10/Rail/main.py
@@ -1,5 +1,3 @@
-# # echo-server.py
-
 import socket
 import time
 
@@ -9,6 +7,7 @@ PORT = 503
 read = 0
 write = 1
 
+# Create TCP/IP connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
@@ -68,8 +67,6 @@ def set_mode(mode):
     sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 96, 0, 0, 0, 0, 1, mode]))
     while (sendCommand(bytearray([0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1])) != [0, 0, 0, 0, 0, 14, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1, mode]):
 
-        print("wait for mode")
-
         #1 second delay
         time.sleep(1)
 
@@ -84,14 +81,6 @@ def startProcedure():
     set_swon()
     set_op_en()
 
-def getReadyToMove():
-    set_mode(1)
-    # # Set velocity 6081h
-    # sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 129, 0, 0, 0, 0, 2, 244, 1]))
-
-    # # Set acceleration 6083h
-    # sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 131, 0, 0, 0, 0, 2, 184, 11]))
-
 def targetPosition(target, rw=1):
 
     def extractBytes(integer):
@@ -101,16 +90,12 @@ def targetPosition(target, rw=1):
     if target > 0xffff:
         print("Invalid target specified")
     else:
-        if target > 255:
+        if target > 255:  # If the target is over 2 bytes large, split the data correctly into two seperate bytes.
             target2Byt = extractBytes(target)
-            targetPos = [0, 0, 0, 0, 0, 15, 0, 43, 13, rw, 0, 0, 96, 122, 0, 0, 0, 0, 4, target2Byt[0], target2Byt[1]]
+            targetPos = [0, 0, 0, 0, 0, 15, 0, 43, 13, rw, 0, 0, 0x60, 0x7a, 0, 0, 0, 0, 2, target2Byt[0], target2Byt[1]]
         elif target <= 255:
-            targetPos = [0, 0, 0, 0, 0, 14, 0, 43, 13, rw, 0, 0, 96, 122, 0, 0, 0, 0, 1, target]
-
-        print(f"targetPos: {targetPos}")
-        #targetPos = [0, 0, 0, 0, 0, 14, 0, 43, 13, rw, 0, 0, 0x60, 0x7a, 0, 0, 0, 0, 1, 250]
+            targetPos = [0, 0, 0, 0, 0, 14, 0, 43, 13, rw, 0, 0, 0x60, 0x7a, 0, 0, 0, 0, 1, target]
         targetPos_array = bytearray(targetPos)
-        print("Sending targetPos_array")
         sendCommand(targetPos_array)
 
         # set velocity and acceleration of profile
@@ -120,29 +105,15 @@ def targetPosition(target, rw=1):
         # Profile deacceleration set below
         sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 0x60, 0x84, 0, 0, 0, 0, 2, 0x2c, 0x1]))
 
-        # # set velocity target
-        # sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 0x60, 0xFF, 0, 0, 0, 0, 2, 0x2c, 0x1]))
-        
-        print("My location:")
-        sendCommand(bytearray([0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 0x60, 0x64, 0, 0, 0, 0, 4]))
-        print("targetpos:")
-        sendCommand(bytearray([0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 0x60, 0x7a, 0, 0, 0, 0, 4]))
-
         # Execute command
         sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, rw, 0, 0, 0x60, 0x40, 0, 0, 0, 0, 2, 0x1f, 0x0]))
 
-        time.sleep(0.1)
-        print("targetpos after execute command:")
-        sendCommand(bytearray([0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 0x60, 0x7a, 0, 0, 0, 0, 4]))
+        time.sleep(0.01)
 
         # Check Statusword for target reached
         while (sendCommand(status_array) != [0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 0x60, 0x41, 0, 0, 0, 0, 2, 39, 22]):
-            print("Wait for next command")
-
-            print("targetpos in loop:")
-            sendCommand(bytearray([0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 0x60, 0x7a, 0, 0, 0, 0, 4]))
             # 1 second delay
-            time.sleep(1)
+            time.sleep(0.01)
 
         sendCommand(enableOperation_array)
 
@@ -151,56 +122,47 @@ def sendCommand(data):
     #Create socket and send request
     s.send(data)
     res = s.recv(24)
-    #Print response telegram
-    print(list(res))
+    # #Print response telegram
+    # print(list(res))
     return list(res)
 
 def homing():
     set_mode(6)
 
-    setHomingMethodLSP = [0, 0, 0, 0, 0, 14, 0, 43, 13, write, 0, 0, 96, 152, 0, 0, 0, 0, 1, 17]
-    setHomingMethodLSP_array = bytearray(setHomingMethodLSP)
-    sendCommand(setHomingMethodLSP_array)
+    setHomingMethodLSN = [0, 0, 0, 0, 0, 14, 0, 43, 13, write, 0, 0, 96, 152, 0, 0, 0, 0, 1, 17]
+    setHomingMethodLSN_array = bytearray(setHomingMethodLSN)
+    sendCommand(setHomingMethodLSN_array)
 
-    # Set 60A8 SI Unit position to multiplying factor 1 (positions and alike are now given in orders of 1mm)
-    # sendCommand(bytearray([0, 0, 0, 0, 0, 17, 0, 43, 13, write, 0, 0, 96, 168, 0, 0, 0, 0, 4, 0, 0, 1, 249]))
+    # Set homing speeds 6099h
+    sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 0x60, 0x99, 0, 0, 0, 0, 1, 200]))
+    sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 0x60, 0x99, 1, 0, 0, 0, 1, 200]))
+    sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 0x60, 0x99, 2, 0, 0, 0, 1, 200]))
 
-    # # Set feed constant 6092h_01h
-    # sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 146, 1, 0, 0, 0, 2, 64, 56]))
-
-    # Set feed revolutions 6092h_02h
-    # sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 146, 2, 0, 0, 0, 1, 1]))
-
-    # # Set homing speeds 6099h
-    sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 0x60, 0x99, 0, 0, 0, 0, 1, 0x02]))
-    sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 0x60, 0x99, 1, 0, 0, 0, 1, 0xfa]))
-    sendCommand(bytearray([0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 0x60, 0x99, 2, 0, 0, 0, 1, 0xc8]))
-
-    # # Set acceleration 609Ah
+    # Set acceleration 609Ah
     sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 0x60, 0x9a, 0, 0, 0, 0, 2, 0xe8, 0x3]))
 
-    time.sleep(1)
-    print("About to home")
+    time.sleep(0.1)
+
+    print("Begin Homing")
     # Start Homing 6040h
     sendCommand(bytearray([0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 0x60, 0x40, 0, 0, 0, 0, 2, 0x1f, 0]))
   
     while (sendCommand(status_array) != [0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 0x60, 0x41, 0, 0, 0, 0, 2, 39, 22]):
-        print("wait for Homing to end")
         # 1 second delay
-        time.sleep(1)
+        time.sleep(0.1)
 
     print("Homing complete")
 
     sendCommand(enableOperation_array)
 
-# def togglePower():
-#     digitalInput7 = [0, 0, 0, 0, 0, 17, 0, 43, 13, write, 0, 0, 32, 16, 0, 0, 0, 0, 4, 0, 0, 0, 102]
-#     digitalInput7_array = bytearray(digitalInput7)
-#     sendCommand(digitalInput7_array)
-#
-# togglePower()
-startProcedure()
-homing()
-getReadyToMove()
-targetPosition(1000)
+def init():
+    startProcedure()
+    homing()
+    set_mode(1)
+
+init()
+
+# Never input target position lower than 1. It will trigger the limit switch.
+
+targetPosition(1)
 
