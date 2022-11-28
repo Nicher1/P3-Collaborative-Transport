18ed158647b74b51d9156a998590094748111459
EliasTDam <edam21@student.aau.dk>
2022-11-28 10:26:05 +0100

    Added function for converting from pixel coordinates to euclidean coordinates.

---
+++ pixelDist2EucDist.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def pixelDist2EucDist(xp, yp, h, FOVx=(np.pi/2), FOVy=1.03, xwidth=1280, yheight=720):
+    # xp = x coordinate in pixels, yp = y coordinate in pixels. h = 3D distance to point.
+    # All angles are in radians
+    thetax = (FOVx/xwidth)*(xp-(xwidth/2))
+    thetay = (FOVy/yheight)*(yp-(yheight/2))
+
+    x = h* np.sin(thetax)
+    y = h*np.sin(thetay)
+    z = h*np.cos(thetax)
+
+    pos = [x, y, z]
+    return pos
