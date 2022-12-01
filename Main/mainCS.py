38a86f7a4b39e4e4a713bf7b029bb80e1fd3ab86
EliasTDam <edam21@student.aau.dk>
2022-12-01 15:01:33 +0100

    Added serverTemplate, which incorperates functions for interperating UDP messages, reading and writing to objects. Added interprating function of UDP messages to mainCS

--- Main/mainCS.py
+++ Main/mainCS.py
@@ -3,28 +3,18 @@ import socket
 
 # Addresses and ports
 localAddress = "127.0.0.1"
-buffersize = 8
+buffersize = 9
 
 class subsys:
-    def __init__(self, CLIENT_PORT, SERVER_PORT, s):
+    def __init__(self, CLIENT_PORT, SERVER_PORT):
         self.CLIENT_PORT = CLIENT_PORT
         self.SERVER_PORT = SERVER_PORT
         self.s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        s.bind((localAddress, self.CLIENT_PORT))
+        self.s.bind((localAddress, self.CLIENT_PORT))
 
 ur10 = subsys(20001, 20002)
 rail = subsys(20003, 20004)
 camera = subsys(20005, 20006)
-
-# Sockets and bindings
-# ur10 = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# ur10.bind((localAddress, ur10PORTS.CLIENT_PORT))
-#
-# rail = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# rail.bind((localAddress, railPORTS.CLIENT_PORT))
-#
-# camera = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# camera.bind((localAddress, cameraPORTS.CLIENT_PORT))
 
 def extractBytes(integer):
     firstArray = divmod(integer, 0x100)
@@ -33,23 +23,42 @@ def extractBytes(integer):
     output = [thirdArray[0], thirdArray[1], secondArray[1], firstArray[1]]
     return output
 
-def communicateUDP(sub_system, object, subindex=0, rw=0, information=0):
+def combineBytes(sign, bytes):
+    output = bytes[0]*256**3 + bytes[1]*256**2 + bytes[2]*256 + bytes[3]
+    if sign == 1:
+        output = output*(-1)
+    return output
+
+def communicateUDP(sub_system, object, subindex=0, rw=0, information=0, nr_of_following_messages=0):
 
     # Format data
     sign = 0
-    if information > 0:
+    if information < 0:
         sign = 1
         information = abs(information)
     information = extractBytes(information)
-    package = [object, subindex, rw, sign, information[0], information[1], information[2], information[3]]
+    package = [object, subindex, rw, sign, information[0], information[1], information[2], information[3], nr_of_following_messages]
     package_array = bytes(package)
 
     # Send data
-    sub_system.sendto(package_array, (localAddress, sub_system.SERVER_PORT))
+    sub_system.s.sendto(package_array, (localAddress, sub_system.SERVER_PORT))
 
     # Recieve data
-    recieved = sub_system.recvfrom(buffersize)
-    recieved = list(recieved)
-    return recieved
+    if rw == 0:
+        recieved = sub_system.s.recvfrom(buffersize)
+        recieved = list(recieved[0])
+        recieved = combineBytes(recieved[3], recieved[4:8])
+        return recieved
 
+communicateUDP(ur10, 1, 1, rw=1, information=100500, nr_of_following_messages=2)
+communicateUDP(ur10, 1, 2, rw=1, information=100400, nr_of_following_messages=1)
+communicateUDP(ur10, 1, 3, rw=1, information=100300, nr_of_following_messages=0)
+
+print(communicateUDP(ur10, 1, 1))
+print(communicateUDP(ur10, 1, 2))
+print(communicateUDP(ur10, 1, 3))
+
+communicateUDP(ur10, 1, 3, rw=1, information=1500)
+
+print(communicateUDP(ur10, 1, 3))
 
