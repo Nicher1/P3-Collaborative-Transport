c16b16437747c995c6c0c4129ab243dafbac6c34
EliasTDam <edam21@student.aau.dk>
2022-12-13 10:45:00 +0100

    Created test data

--- Main/mainCS.py
+++ Main/mainCS.py
@@ -127,13 +127,10 @@ while True:
                                     [0, 1, 0, 0],
                                     [0, 0, 1, 0],
                                     [0, 0, 0, 1]])
-    camera_data = open("DepthTest1.txt", "a")
+    camera_data = open("DepthTest5.txt", "a")
 
     while True:
         # Step 1: Update all variables (attain current position, and human position from camera)
-        camera_vec[0] = communicateUDP(ur10, 2, 3, nr_of_following_messages=5) #Read the orientation of the robot
-        camera_vec[1] = communicateUDP(ur10, 2, 4, nr_of_following_messages=4)
-        camera_vec[2] = communicateUDP(ur10, 2, 5, nr_of_following_messages=3)
 
         T_robotbase_EE[0, 3] = communicateUDP(ur10, 1, 0, nr_of_following_messages=2)  # Update UR10 position
         T_robotbase_EE[1, 3] = communicateUDP(ur10, 1, 1, nr_of_following_messages=1)
